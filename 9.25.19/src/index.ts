1f83d3842dcba2dee2b06a9dd93c23a3aa46efc9
BigBrownAfro <bigbrownafro@gmail.com>
2019-09-27 14:47:39 -0600

    Added mouse listener in an index.ts for the canvas and some functions.

--- 9.25.19/src/index.ts
+++ 9.25.19/src/index.ts
@@ -1,54 +1,74 @@
-interface ICustomer{
-    id: string;
-    name: string;
-    username: string;
-    email: string;
-    address: string;
-    phone: string;
-    website: string;
-    company: string;
-    avatar: string;
-}
-
 export class Main{
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+    private flag = false;
+    private previousX = 0;
+    private currentX = 0;
+    private previousY = 0;
+    private currentY = 0;
+    private dotFlag = false;
+    private fillColor = "black";
+    private stroke = 2;
+    private width: number;
+    private height: number;
 
     constructor(){
-        const button = document.getElementById("populate") as HTMLButtonElement;
-        button.onclick = () => this.populateData();
+        this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        this.context = this.canvas.getContext("2d");
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+
+        this.canvas.addEventListener("mousemove",(e: MouseEvent) => {
+            this.findxy("move", e);
+        });
     }
 
-    public async populateData(){
-        let tableBody = document.getElementById("table-body");
-        let url = "https://cs4350.herokuapp.com/demo/all";
-        let customerData = await this.loadData(url);
-        tableBody.innerHTML = this.createHtmlFromArray(customerData);
-        console.log("populate data");
-    }
-
-    public async loadData(url: string): Promise<ICustomer[]>{
-        const response = await fetch(url);
-        const json = await response.json();
-        return json as ICustomer[];
-    }
-
-    public createHtmlFromArray(dataArray: ICustomer[]): string {
-        let listHtml = "";
-        let template = document.getElementById("table-template");
-        let templateHtml = template.innerHTML;
-
-        for(let index = 0; index < dataArray.length; index++){
-            listHtml += templateHtml
-                                    .replace(/{{id}}/g, dataArray[index].id)
-                                    .replace(/{{name}}/g, dataArray[index].name)
-                                    .replace(/{{username}}/g, dataArray[index].username)
-                                    .replace(/{{email}}/g, dataArray[index].email)
-                                    .replace(/{{address}}/g, dataArray[index].address)
-                                    .replace(/{{phone}}/g, dataArray[index].phone)
-                                    .replace(/{{website}}/g, dataArray[index].website)
-                                    .replace(/{{company}}/g, dataArray[index].company)
-                                    .replace(/{{avatar}}/g, dataArray[index].avatar)
+    public color(color: string): void {
+        this.fillColor = color;
+        if (this.fillColor === "white"){
+            this.stroke = 20;
+        }else{
+            this.stroke = 2;
         }
-        return listHtml;
+    }
+
+    public draw(){
+        this.context.beginPath();
+        this.context.moveTo(this.previousX, this.previousY);
+        this.context.lineTo(this.currentX, this.currentY);
+        this.context.strokeStyle = this.fillColor;
+        this.context.lineWidth = this.stroke;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    public clear(){
+        const clearIsConfirmed = confirm("Are you sure you want to clear?");
+        if(clearIsConfirmed){
+            this.context.clearRect(0,0,this.width,this.height);
+            document.getElementById("canvasimg").style.display = "none";
+        }
+    }
+
+    public save(){
+        const img = document.getElementById("canvasimg") as HTMLImageElement;
+        img.style.border = "2px solid";
+        const dataUrl = this.canvas.toDataURL();
+        img.src = dataUrl;
+        img.style.display = "inline";
+    }
+
+    public findxy(eventType: string, e: MouseEvent): void {
+        console.log(eventType);
+        if(eventType === "down"){
+
+        }
+        if(eventType === 'up' || eventType === "out"){
+
+        }
+        if(eventType === "move"){
+
+        }
     }
 }
 
