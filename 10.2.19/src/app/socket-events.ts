c93a837fe37b605b746cc9317f72a168e27f6684
BigBrownAfro <bigbrownafro@gmail.com>
2019-10-02 14:47:01 -0600

    Started work with angular server

---
+++ 10.2.19/src/app/socket-events.ts
@@ -0,0 +1,10 @@
+export const SocketEvent = {
+    AddUser: 'add user',
+    NewMessage: 'new message',
+    Login: 'login',
+    UserJoin: 'user joined',
+    UserLeft: 'user left',
+    Disconnect: 'disconnect',
+    Reconnect: 'reconnect',
+    ReconnectError: 'reconnect_error'
+};
